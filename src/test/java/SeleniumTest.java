bf0e1c13eb8404c08293e3fe3e7faa95e1da5b02
Andrzej Pawelec <andrzej.pawelec@enigma.com.pl>
2024-07-16 09:42:27 +0200

    SeleniumTest run browsers update

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,20 +1,46 @@
+import org.openqa.selenium.InvalidArgumentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
+
+import java.util.Scanner;
 
 public class SeleniumTest {
 
-    public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
-
-        System.out.println("treœæ");
-    }
+//  driver.get("https://www.wp.pl");
 
     @Test
-    public void sampleTest() {
-        int x = 1;
-        System.out.println("treœæ");
+    public void openGooglePage() {
+        WebDriver driver = getDriver("firefox");
+        driver.get("https://google.pl");
     }
 
 
-}
+    public WebDriver getDriver(String browser) {
+
+
+        switch (browser) {
+            case "firefox":
+                return new FirefoxDriver();
+            case "chrome":
+                return new ChromeDriver();
+            default:
+                 throw new InvalidArgumentException("Invalid browser name");
+        }
+    }
+
+
+    public WebDriver getDriverIf(String browser) {
+
+        if (browser == "chrome") {
+            return new ChromeDriver();
+        } else if (browser == "firefox") {
+            return new FirefoxDriver();
+        } else {
+            throw new InvalidArgumentException("Invalid browser name");
+        }
+    }
+    }
+
+
